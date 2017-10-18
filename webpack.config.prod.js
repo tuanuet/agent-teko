48fa902a8fcb4361c9d4a18c38981c6d1571541c
tuan.vm <tuan.uet@gmail.com>
2017-10-18 17:13:46 +0700

    webpack server

---
+++ webpack.config.prod.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const webpack = require('webpack');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = {
+    entry: [
+        'babel-polyfill', // Necessary for hot reloading with IE
+        './src/client/index'
+    ],
+    // output: {
+    //     path: path.join(__dirname, 'static'),
+    //     filename: 'bundle.js',
+    //     publicPath: '/'
+    // },
+    // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
+    output: {
+        filename: '[name]-bundle.js',
+        chunkFilename: '[name]-chunk.js',
+        path: path.resolve(__dirname, 'static'),
+        publicPath: 'react-redux/dist/'
+    },
+    plugins: [
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'node-static',
+            filename: 'node-static.js',
+            minChunks(module, count) {
+                var context = module.context;
+                return context && context.indexOf('node_modules') >= 0;
+            },
+        }),
+
+        //*********************************** async chunks*************************
+
+        //catch all - anything used in more than one place
+        new webpack.optimize.CommonsChunkPlugin({
+            async: true,
+            children: true,
+            minChunks(module, count) {
+                return count >= 2;
+            },
+        }),
+
+        //specifically bundle these large things
+        new webpack.optimize.CommonsChunkPlugin({
+            async: true,
+            children: true,
+            minChunks(module, count) {
+                var context = module.context;
+                var targets = ['react-dnd', 'react-dnd-html5-backend', 'react-dnd-touch-backend', 'dnd-core']
+                return context && context.indexOf('node_modules') >= 0 && targets.find(t => new RegExp('\\\\' + t + '\\\\', 'i').test(context));
+            },
+        }),
+
+
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['es2015','react']
+                }
+            },
+            {
+                test: /\.s?css$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.woff|\.svg|\.ttf|\.eot|\.woff2/, loader: 'url-loader'
+            }, {
+                test: /\.json$/,
+                loader: 'json-loader',
+            },
+        ]
+
+    },
+    performance: {
+        assetFilter: (assetFilename) => !(/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename)),
+    },
+};
