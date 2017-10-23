296b4041d03cb5267d1d8f0a7cd3dce055f9995c
tuan.vm <tuan.uet@gmail.com>
2017-10-23 14:38:57 +0700

    icon attachment

--- src/client/components/Message/Attachment.js
+++ src/client/components/Message/Attachment.js
@@ -3,14 +3,12 @@ import React from 'react';
 export default class Attachment extends React.Component {
     render() {
         let message = this.props.message;
+
         let role = message.messageFrom === 0 ? 'self' : 'other';
         return (
-            <div className={role}>
-                <div className="image">
-                    <div className="content">
-                        <div><i class="fa fa-paperclip" aria-hidden="true"></i><a href={message.content}>{message.name}</a>
-                        </div>
-                    </div>
+            <div>
+                <div className={`chat-group ${role}`}>
+                    <div className="chat"><img href="/images/attachment.png"/><a href={message.content}>{message.name}</a></div>
                 </div>
             </div>
         );
