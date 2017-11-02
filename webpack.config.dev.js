6a5635a1d4c3ae1fe04bb141cfac7c975993dc2f
Lê Hải Nam <nam.lh@teko.vn>
2017-11-02 09:27:31 +0700

    Add building command for dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,63 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: [
-        'babel-polyfill', // Necessary for hot reloading with IE
         './src/client/index'
     ],
-    // output: {
-    //     path: path.join(__dirname, 'static'),
-    //     filename: 'bundle.js',
-    //     publicPath: '/'
-    // },
-    // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
     output: {
-        filename: '[name]-bundle.js',
-        chunkFilename: '[name]-chunk.js',
+        filename: 'main-bundle.js',
         path: path.resolve(__dirname, 'static'),
         publicPath: '/'
     },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new BundleAnalyzerPlugin({
-            analyzerMode: 'static'
-        }),
-
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'node-static',
-            filename: 'node-static.js',
-            minChunks(module, count) {
-                var context = module.context;
-                return context && context.indexOf('node_modules') >= 0;
-            },
-        }),
-
-        //*********************************** async chunks*************************
-
-        //catch all - anything used in more than one place
-        new webpack.optimize.CommonsChunkPlugin({
-            async: true,
-            children: true,
-            minChunks(module, count) {
-                return count >= 2;
-            },
-        }),
-
-        //specifically bundle these large things
-        new webpack.optimize.CommonsChunkPlugin({
-            async: true,
-            children: true,
-            minChunks(module, count) {
-                var context = module.context;
-                var targets = ['react-dnd', 'react-dnd-html5-backend', 'react-dnd-touch-backend', 'dnd-core']
-                return context && context.indexOf('node_modules') >= 0 && targets.find(t => new RegExp('\\\\' + t + '\\\\', 'i').test(context));
-            },
-        }),
-
-    ],
-
     module: {
         loaders: [
             {
@@ -80,8 +33,5 @@ module.exports = {
             },
         ]
 
-    },
-    performance: {
-        assetFilter: (assetFilename) => !(/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename)),
     },
 };
