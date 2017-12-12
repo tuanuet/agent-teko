8a992b2244953381adf326e2ef45d373bc093ac9
Lê Hải Nam <nam.lh@teko.vn>
2017-12-12 17:33:33 +0700

    Update check link

--- src/components/Message/Default.js
+++ src/components/Message/Default.js
@@ -11,9 +11,14 @@ export default class Default extends React.Component {
                     <div className={`chat ${message.isError ? `faded` : ``}`}
                         title={message.createdAt}>
                         <div className="sender-name">{message.senderName}</div>
-                        { message.content.startsWith(`https://`) || message.content.startsWith(`http://`)
-                            ? <a href={message.content} target="_blank">{message.content}</a>
-                            : (message.content || '') }
+                        { message.content ? message.content.trim().split(' ').map((word, idx, { length }) => {
+                            const space = idx !== length - 1 ? ' ' : ''
+                            if (word.startsWith(`https://`) || word.startsWith(`http://`)) {
+                                return <a key={idx} href={word} target="_blank">{word}{space}</a>
+                            } else {
+                                return `${word}${space}`
+                            }
+                        }) : '' }
                         { message.isError && <div className="error-message">
                             <i class="fa fa-exclamation" aria-hidden="true"></i>
                             Có lỗi xảy ra: FB không cho phép tin nhắn này tới khách hàng
